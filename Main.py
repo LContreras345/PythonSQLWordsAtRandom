038281b5cf303831b7ec9b1df192bbcf857a3730
LContreras678 <luicacoustic@gmail.com>
2025-09-02 22:00:37 +0100

    git commit -m

--- Main.py
+++ Main.py
@@ -1,8 +1,14 @@
 class MainMenu:
     def __init__(self, displaymenu):
         print("Hello! Welcome to the Random Word Generator")
+        print("Select your option:")
         print("===========================================")
-        print("===========================================")
+        print("1. Word of the day")
+
+class WordOfTheDay:
+    print("The word of the day is:")
+
+
 
 displaymenu = MainMenu("")
 
